ffd8843c97f03dcceb5a5f2518cb10a82f8c646e
Haze2670 <164275205+Haze2670@users.noreply.github.com>
2024-07-19 01:51:06 +0800

    Update Assessment.sol

--- Assessment.sol
+++ Assessment.sol
@@ -5,8 +5,8 @@ contract Assessment {
     address payable public owner;
     uint256 public balance = 1000 ether;
 
-    event Deposit(address indexed account, uint256 amount);
-    event Withdraw(address indexed account, uint256 amount);
+    event Contribution(address indexed account, uint256 amount);
+    event ExtractChange(address indexed account, uint256 amount);
 
     constructor() payable {
         owner = payable(msg.sender);
@@ -16,18 +16,18 @@ contract Assessment {
         return balance;
     }
 
-    function deposit(uint256 _amount) public payable {
+    function contribute(uint256 _amount) public payable {
         require(msg.sender == owner, "You are not the owner of this account");
         
         balance += _amount;
-        emit Deposit(msg.sender, _amount);
+        emit Contribution(msg.sender, _amount);
     }
 
-    function withdraw(uint256 _withdrawAmount) public {
+    function extractChange(uint256 _changeAmount) public {
         require(msg.sender == owner, "You are not the owner of this account");
-        require(_withdrawAmount <= balance, "Insufficient balance");
+        require(_changeAmount <= balance, "Insufficient balance");
 
-        balance -= _withdrawAmount;
-        emit Withdraw(msg.sender, _withdrawAmount);
+        balance -= _changeAmount;
+        emit ExtractChange(msg.sender, _changeAmount);
     }
 }
